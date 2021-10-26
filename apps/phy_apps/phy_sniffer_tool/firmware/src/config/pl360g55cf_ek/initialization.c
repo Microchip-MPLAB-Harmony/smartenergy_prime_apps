99fa5b2ba041bd940c43cfdb2a699d0027445bb3
Cristian Guemes <cristian.guemes@microchip.com>
2021-10-26 10:04:38 +0200

    SMS-2433: Apps: PRIME PHY Sniffer Tool (pl360g55_cf, same70 Xult + pl460-ek)

---
+++ apps/phy_apps/phy_sniffer_tool/firmware/src/config/pl360g55cf_ek/initialization.c
@@ -0,0 +1,340 @@
+/*******************************************************************************
+  System Initialization File
+
+  File Name:
+    initialization.c
+
+  Summary:
+    This file contains source code necessary to initialize the system.
+
+  Description:
+    This file contains source code necessary to initialize the system.  It
+    implements the "SYS_Initialize" function, defines the configuration bits,
+    and allocates any necessary global system resources,
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+#include "configuration.h"
+#include "definitions.h"
+#include "device.h"
+
+
+
+// ****************************************************************************
+// ****************************************************************************
+// Section: Configuration Bits
+// ****************************************************************************
+// ****************************************************************************
+#pragma config SECURITY_BIT = CLEAR
+#pragma config BOOT_MODE = SET
+
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Driver Initialization Data
+// *****************************************************************************
+// *****************************************************************************
+// <editor-fold defaultstate="collapsed" desc="DRV_PLC_HAL Initialization Data">
+
+/* HAL Interface Initialization for PLC transceiver */
+DRV_PLC_PLIB_INTERFACE drvPLCPlib = {
+
+     /* SPI Transfer Setup */
+    .spiPlibTransferSetup = (DRV_PLC_SPI_PLIB_TRANSFER_SETUP)FLEXCOM3_SPI_TransferSetup,
+
+    /* SPI Write/Read */
+    .spiWriteRead = FLEXCOM3_SPI_WriteRead,
+    
+    /* SPI Is Busy */
+    .spiIsBusy = FLEXCOM3_SPI_IsBusy,
+    
+    /* SPI clock frequency */
+    .spiClockFrequency = DRV_PLC_SPI_CLK,
+    
+    /* PLC LDO Enable Pin */
+    .ldoPin = DRV_PLC_LDO_EN_PIN, 
+    
+    /* PLC Reset Pin */
+    .resetPin = DRV_PLC_RESET_PIN,
+       
+    /* PLC External Interrupt Pin */
+    .extIntPin = DRV_PLC_EXT_INT_PIN,
+
+    /* PLC External Interrupt Pin */
+    .cdPin = DRV_PLC_CD_PIN,
+
+};
+
+/* HAL Interface Initialization for PLC transceiver */
+DRV_PLC_HAL_INTERFACE drvPLCHalAPI = {
+
+    /* PLC PLIB */
+    .plcPlib = &drvPLCPlib,
+
+    /* PLC HAL init */
+    .init = (DRV_PLC_HAL_INIT)DRV_PLC_HAL_Init,
+
+    /* PLC HAL setup */
+    .setup = (DRV_PLC_HAL_SETUP)DRV_PLC_HAL_Setup,
+
+    /* PLC transceiver reset */
+    .reset = (DRV_PLC_HAL_RESET)DRV_PLC_HAL_Reset,
+
+    /* PLC Carrier Detect Status */
+    .getCarrierDetect = (DRV_PLC_HAL_GET_CD)DRV_PLC_HAL_GetCarrierDetect,
+
+    /* PLC HAL Enable/Disable external interrupt */
+    .enableExtInt = (DRV_PLC_HAL_ENABLE_EXT_INT)DRV_PLC_HAL_EnableInterrupts,
+
+    /* PLC HAL delay function */
+    .delay = (DRV_PLC_HAL_DELAY)DRV_PLC_HAL_Delay,
+
+    /* PLC HAL Transfer Bootloader Command */
+    .sendBootCmd = (DRV_PLC_HAL_SEND_BOOT_CMD)DRV_PLC_HAL_SendBootCmd,
+
+    /* PLC HAL Transfer Write/Read Command */
+    .sendWrRdCmd = (DRV_PLC_HAL_SEND_WRRD_CMD)DRV_PLC_HAL_SendWrRdCmd,
+};
+
+// </editor-fold>
+// <editor-fold defaultstate="collapsed" desc="DRV_PLC_PHY Initialization Data">
+
+/* PLC Binary file addressing */
+extern uint8_t plc_phy_bin_start;
+extern uint8_t plc_phy_bin_end;
+
+/* PLC Driver Initialization Data */
+DRV_PLC_PHY_INIT drvPlcPhyInitData = {
+
+    /* SPI PLIB API  interface*/
+    .plcHal = &drvPLCHalAPI,
+
+    /* PLC PHY Number of clients */
+    .numClients = DRV_PLC_PHY_CLIENTS_NUMBER_IDX,  
+
+    /* PLC PHY profile */
+    .plcProfile = DRV_PLC_PHY_PROFILE,
+ 
+    /* PLC Binary start address */
+    .binStartAddress = (uint32_t)&plc_phy_bin_start,
+    
+    /* PLC Binary end address */
+    .binEndAddress = (uint32_t)&plc_phy_bin_end,
+
+    /* Secure Mode */
+    .secure = DRV_PLC_SECURE,
+    
+};
+
+// </editor-fold>
+// <editor-fold defaultstate="collapsed" desc="SRV_USI Instance 0 Initialization Data">
+
+static uint8_t CACHE_ALIGN srvUSI0ReadBuffer[SRV_USI0_RD_BUF_SIZE] = {0};
+static uint8_t CACHE_ALIGN srvUSI0WriteBuffer[SRV_USI0_WR_BUF_SIZE] = {0};
+
+static uint8_t CACHE_ALIGN srvUSI0CDCReadBuffer[128] = {0};
+
+/* Declared in USI CDC service implementation (srv_usi_cdc.c) */
+extern const SRV_USI_DEV_DESC srvUSICDCDevDesc;
+
+const USI_CDC_INIT_DATA srvUsi0InitData = {
+    .cdcInstanceIndex = 0,
+    .usiReadBuffer = srvUSI0ReadBuffer,
+    .usiBufferSize = SRV_USI0_RD_BUF_SIZE,
+    .cdcReadBuffer = srvUSI0CDCReadBuffer,
+    .cdcBufferSize = 128
+};
+
+const SRV_USI_INIT srvUSI0Init =
+{
+    .deviceInitData = (const void*)&srvUsi0InitData,
+    .consDevDesc = &srvUSICDCDevDesc,
+    .deviceIndex = 0,
+    .pWrBuffer = srvUSI0WriteBuffer,
+    .wrBufferSize = SRV_USI0_WR_BUF_SIZE
+};
+
+
+// </editor-fold>
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Data
+// *****************************************************************************
+// *****************************************************************************
+/* Structure to hold the object handles for the modules in the system. */
+SYSTEM_OBJECTS sysObj;
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Library/Stack Initialization Data
+// *****************************************************************************
+// *****************************************************************************
+/******************************************************
+ * USB Driver Initialization
+ ******************************************************/
+ 
+static DRV_USB_VBUS_LEVEL DRV_USBDP_VBUS_Comparator(void)
+{
+    DRV_USB_VBUS_LEVEL retVal = DRV_USB_VBUS_LEVEL_INVALID;
+    if(true == USB_VBUS_SENSE_Get())
+    {
+        retVal = DRV_USB_VBUS_LEVEL_VALID;
+    }
+	return (retVal);
+
+}
+
+const DRV_USBDP_INIT drvUSBInit =
+{
+    /* Interrupt Source for USB module */
+    .interruptSource = UDP_IRQn,
+
+    /* System module initialization */
+    .moduleInit = {0},
+
+    /* To operate in USB Normal Mode */
+	.operationSpeed = USB_SPEED_FULL,
+
+    /* Identifies peripheral (PLIB-level) ID */
+    .usbID = UDP_REGS,
+	
+    /* Function to check for VBus */
+    .vbusComparator = DRV_USBDP_VBUS_Comparator
+};
+
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Initialization
+// *****************************************************************************
+// *****************************************************************************
+// <editor-fold defaultstate="collapsed" desc="SYS_TIME Initialization Data">
+
+const SYS_TIME_PLIB_INTERFACE sysTimePlibAPI = {
+    .timerCallbackSet = (SYS_TIME_PLIB_CALLBACK_REGISTER)TC0_CH0_TimerCallbackRegister,
+    .timerStart = (SYS_TIME_PLIB_START)TC0_CH0_TimerStart,
+    .timerStop = (SYS_TIME_PLIB_STOP)TC0_CH0_TimerStop ,
+    .timerFrequencyGet = (SYS_TIME_PLIB_FREQUENCY_GET)TC0_CH0_TimerFrequencyGet,
+    .timerPeriodSet = (SYS_TIME_PLIB_PERIOD_SET)TC0_CH0_TimerPeriodSet,
+    .timerCompareSet = (SYS_TIME_PLIB_COMPARE_SET)TC0_CH0_TimerCompareSet,
+    .timerCounterGet = (SYS_TIME_PLIB_COUNTER_GET)TC0_CH0_TimerCounterGet,
+};
+
+const SYS_TIME_INIT sysTimeInitData =
+{
+    .timePlib = &sysTimePlibAPI,
+    .hwTimerIntNum = TC0_CH0_IRQn,
+};
+
+// </editor-fold>
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Local initialization functions
+// *****************************************************************************
+// *****************************************************************************
+
+
+
+/*******************************************************************************
+  Function:
+    void SYS_Initialize ( void *data )
+
+  Summary:
+    Initializes the board, services, drivers, application and other modules.
+
+  Remarks:
+ */
+
+void SYS_Initialize ( void* data )
+{
+
+
+    EFC_Initialize();
+  
+	PIO_Initialize();
+
+    CLOCK_Initialize();
+
+
+
+	WDT_Initialize();
+
+
+    FLEXCOM3_SPI_Initialize();
+
+  
+
+ 
+    TC0_CH0_TimerInitialize(); 
+     
+    
+	BSP_Initialize();
+
+    /* Initialize PLC Phy Driver Instance */
+    sysObj.drvPlcPhy = DRV_PLC_PHY_Initialize(DRV_PLC_PHY_INDEX, (SYS_MODULE_INIT *)&drvPlcPhyInitData);
+    /* Register Callback function to handle PLC interruption */
+    PIO_PinInterruptCallbackRegister((PIO_PIN)DRV_PLC_EXT_INT_PIN, DRV_PLC_PHY_ExternalInterruptHandler, sysObj.drvPlcPhy);
+    /* Initialize USI Service Instance 0 */
+    sysObj.srvUSI0 = SRV_USI_Initialize(SRV_USI_INDEX_0, (SYS_MODULE_INIT *)&srvUSI0Init);
+
+    sysObj.sysTime = SYS_TIME_Initialize(SYS_TIME_INDEX_0, (SYS_MODULE_INIT *)&sysTimeInitData);
+
+	/* Initialize USB Driver */ 
+    sysObj.drvUSBDPObject = DRV_USBDP_Initialize(DRV_USBDP_INDEX_0, (SYS_MODULE_INIT *) &drvUSBInit);	
+
+
+	 /* Initialize the USB device layer */
+    sysObj.usbDevObject0 = USB_DEVICE_Initialize (USB_DEVICE_INDEX_0 , ( SYS_MODULE_INIT* ) & usbDevInitData);
+	
+	
+
+
+    APP_Initialize();
+
+
+    NVIC_Initialize();
+
+}
+
+
+/*******************************************************************************
+ End of File
+*/
