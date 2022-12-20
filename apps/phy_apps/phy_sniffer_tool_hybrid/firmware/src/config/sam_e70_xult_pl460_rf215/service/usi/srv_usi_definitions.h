98615a8d9977062316cf47fda3fd40138ea00933
Luis Lacasa <luis.lacasa@microchip.com>
2022-12-20 09:10:51 +0100

    SMS-2824: Rename config folder in phy_sniffer_tool_hybrid for SAME70

---
+++ apps/phy_apps/phy_sniffer_tool_hybrid/firmware/src/config/sam_e70_xult_pl460_rf215/service/usi/srv_usi_definitions.h
@@ -0,0 +1,175 @@
+/*******************************************************************************
+  USI Service Definitions Header File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    srv_usi_definitions.h
+
+  Summary:
+    USI Service Definitions Header File
+
+  Description:
+    This file provides implementation-specific definitions for the USI
+    service's system interface.
+*******************************************************************************/
+
+//DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2021 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+//DOM-IGNORE-END
+
+#ifndef SRV_USI_DEFINITIONS_H
+#define SRV_USI_DEFINITIONS_H
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: File includes
+// *****************************************************************************
+// *****************************************************************************
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+
+    extern "C" {
+
+#endif
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Data Types
+// *****************************************************************************
+// *****************************************************************************
+
+// *****************************************************************************
+/* USI Service Serial Protocol Identifiers
+
+  Summary:
+    List of available USI protocols.
+
+  Description:
+    This data type defines the identifier required to specify the protocol to 
+    use by USI service.
+
+  Remarks:
+    None.
+*/
+
+typedef enum
+{
+    /* PRIME Manager generic */
+    SRV_USI_PROT_ID_MNGP_PRIME                = 0x00,
+
+    /* PRIME Manager: GETQRY */
+    SRV_USI_PROT_ID_MNGP_PRIME_GETQRY         = 0x00,
+
+    /* PRIME Manager: GETRSP */
+    SRV_USI_PROT_ID_MNGP_PRIME_GETRSP         = 0x01,
+
+    /* PRIME Manager: SET */
+    SRV_USI_PROT_ID_MNGP_PRIME_SET            = 0x02,
+
+    /* PRIME Manager: RESET */
+    SRV_USI_PROT_ID_MNGP_PRIME_RESET          = 0x03,
+
+    /* PRIME Manager: REBOOT */
+    SRV_USI_PROT_ID_MNGP_PRIME_REBOOT         = 0x04,
+
+    /* PRIME Manager: Firmware Upgrade */
+    SRV_USI_PROT_ID_MNGP_PRIME_FU             = 0x05,
+
+    /* PRIME Manager: GETQRY enhanced */
+    SRV_USI_PROT_ID_MNGP_PRIME_GETQRY_EN      = 0x06,
+
+    /* PRIME Manager: GETRSP enhanced */
+    SRV_USI_PROT_ID_MNGP_PRIME_GETRSP_EN      = 0x07,
+
+    /* PRIME Sniffer */
+    SRV_USI_PROT_ID_SNIF_PRIME                = 0x13,
+
+    /* PRIME PHY Serial */
+    SRV_USI_PROT_ID_PHY_SERIAL_PRIME          = 0x1F,
+
+    /* Physical Layer  */
+    SRV_USI_PROT_ID_PHY                       = 0x22,
+
+    /* G3 Sniffer  */
+    SRV_USI_PROT_ID_SNIFF_G3                  = 0x23,
+
+    /* G3 MAC layer  */
+    SRV_USI_PROT_ID_MAC_G3                    = 0x24,
+
+    /* G3 ADP layer  */
+    SRV_USI_PROT_ID_ADP_G3                    = 0x25,
+
+    /* G3 Coordinator  */
+    SRV_USI_PROT_ID_COORD_G3                  = 0x26,
+
+    /* MicroPLC Physical Layer  */
+    SRV_USI_PROT_ID_PHY_MICROPLC              = 0x27,
+
+    /* RF215 Physical Layer */
+    SRV_USI_PROT_ID_PHY_RF215                 = 0x28,
+
+    /* PRIME API  */
+    SRV_USI_PROT_ID_PRIME_API                 = 0x30,
+
+    /* Invalid protocol  */
+    SRV_USI_PROT_ID_INVALID                   = 0xFF
+
+} SRV_USI_PROTOCOL_ID;
+
+// *****************************************************************************
+/* USI Service Serial Protocol Identifiers
+
+  Summary:
+    List of possible values of USI status.
+
+  Description:
+    This type defines the possible return values for SRV_USI_Status function.
+
+  Remarks:
+    None.
+*/
+
+typedef enum
+{
+    SRV_USI_STATUS_UNINITIALIZED,
+
+    SRV_USI_STATUS_NOT_CONFIGURED,
+
+    SRV_USI_STATUS_CONFIGURED,
+
+    SRV_USI_STATUS_ERROR
+
+} SRV_USI_STATUS;
+
+//DOM-IGNORE-BEGIN
+#ifdef __cplusplus
+}
+#endif
+//DOM-IGNORE-END
+
+#endif // #ifndef SRV_USI_DEFINITIONS_H
