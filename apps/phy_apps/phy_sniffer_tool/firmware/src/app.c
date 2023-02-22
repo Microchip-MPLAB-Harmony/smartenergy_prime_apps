04bf5261eceaa9ba1b9019da6faa710023217314
Luis Lacasa <luis.lacasa@microchip.com>
2023-02-22 12:32:52 +0100

    SMS-3284: Regenerate phy_sniffer_tool projects after PLC Driver bootloader enhancement

--- apps/phy_apps/phy_sniffer_tool/firmware/src/app.c
+++ apps/phy_apps/phy_sniffer_tool/firmware/src/app.c
@@ -119,6 +119,7 @@ static void APP_PLCDataIndCb(DRV_PLC_PHY_RECEPTION_OBJ *indObj, uintptr_t contex
 
         /* Turn on indication LED and start timer to turn it off */
         SYS_TIME_TimerDestroy(appPlcData.tmr2Handle);
+        appPlcData.tmr2Expired = false;
         USER_PLC_IND_LED_On();
         appPlcData.tmr2Handle = SYS_TIME_CallbackRegisterMS(APP_Timer2_Callback, 0,
                 LED_BLINK_PLC_MSG_MS, SYS_TIME_SINGLE);
