c7b65631d5cdde3cbe3ef31eb127970d987c5b40
Luis Lacasa <luis.lacasa@microchip.com>
2023-02-23 14:31:33 +0100

    SMS-3288: Create phy_sniffer_tool_hybrid project for PIC32CXMTG-EK, replacing the one for PIC32CXMTSH-DB

---
+++ apps/phy_apps/phy_sniffer_tool_hybrid/firmware/src/config/pic32cx_mtg_ek_pl460_rf215/user.h
@@ -0,0 +1,55 @@
+/*******************************************************************************
+  User Configuration Header
+
+  File Name:
+    user.h
+
+  Summary:
+    Build-time configuration header for the user defined by this project.
+
+  Description:
+    An MPLAB Project may have multiple configurations.  This file defines the
+    build-time options for a single configuration.
+
+  Remarks:
+    It only provides macro definitions for build-time configuration options
+
+*******************************************************************************/
+
+#ifndef USER_H
+#define USER_H
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+
+extern "C" {
+
+#endif
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: User Configuration macros
+// *****************************************************************************
+// *****************************************************************************
+
+#define USER_BLINK_LED_On()           LED_GREEN_On()
+#define USER_BLINK_LED_Off()          LED_GREEN_Off()
+#define USER_BLINK_LED_Toggle()       LED_GREEN_Toggle()
+
+#define USER_PLC_IND_LED_On()         LED_BLUE_On()
+#define USER_PLC_IND_LED_Off()        LED_BLUE_Off()
+#define USER_PLC_IND_LED_Toggle()     LED_BLUE_Toggle()
+
+#define CLEAR_WATCHDOG()              DWDT_WDT0_Clear()
+
+//DOM-IGNORE-BEGIN
+#ifdef __cplusplus
+}
+#endif
+//DOM-IGNORE-END
+
+#endif // USER_H
+/*******************************************************************************
+ End of File
+*/
