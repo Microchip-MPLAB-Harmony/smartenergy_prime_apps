dfa4b89ca33203590b328e168eb12ab2af3193fd
Blanca Melguizo <blanca.melguizo@microchip.com>
2024-10-22 10:57:38 +0200

    SMS-4055: BN libraries regenerated and stored with correct GIT attributes.

--- apps/prime_apps/prime_dual_service_modem/firmware/src/config/pic32cx_mtg_ek_pl460_rf215/definitions.h
+++ apps/prime_apps/prime_dual_service_modem/firmware/src/config/pic32cx_mtg_ek_pl460_rf215/definitions.h
@@ -50,8 +50,8 @@
 #include <stdbool.h>
 #include "service/pcoup/srv_pcoup.h"
 #include "peripheral/flexcom/usart/plib_flexcom7_usart.h"
-#include "peripheral/adc/plib_adc.h"
 #include "peripheral/flexcom/spi/master/plib_flexcom3_spi_master.h"
+#include "peripheral/adc/plib_adc.h"
 #include "driver/rf215/drv_rf215.h"
 #include "peripheral/flexcom/spi/master/plib_flexcom5_spi_master.h"
 #include "peripheral/tc/plib_tc0.h"
@@ -59,10 +59,10 @@
 #include "service/usi/srv_usi_usart.h"
 #include "peripheral/flexcom/usart/plib_flexcom0_usart.h"
 #include "service/log_report/srv_log_report.h"
-#include "system/time/sys_time.h"
 #include "driver/plc/phy/drv_plc_phy_definitions.h"
 #include "driver/plc/phy/drv_plc_phy.h"
 #include "driver/plc/phy/drv_plc_phy_comm.h"
+#include "system/time/sys_time.h"
 #include "service/reset_handler/srv_reset_handler.h"
 #include "peripheral/trng/plib_trng.h"
 #include "service/rsniffer/srv_rsniffer.h"
@@ -233,9 +233,9 @@ Remarks:
 typedef struct
 {
     SYS_MODULE_OBJ drvRf215;
-    SYS_MODULE_OBJ  sysTime;
 
     SYS_MODULE_OBJ drvPlcPhy;
+    SYS_MODULE_OBJ  sysTime;
     SYS_MODULE_OBJ  sysConsole0;
 
     SYS_MODULE_OBJ  sysDebug;
